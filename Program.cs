6e4e1613621d298a7e63c72ad1ecfdab836431c4
Baran Unsal <barannunsal@gmail.com>
2022-07-12 15:16:10 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Lütfen metin veya kelime giriniz: ");
+string kelime = Console.ReadLine();
+string son = ReverseString(kelime);
+Console.WriteLine($"Girdiğiniz değer: {kelime}\nSonuç: {son}");
+
+string ReverseString(string kelime)
+{
+    char[] charArr = kelime.ToCharArray();
+    string reverseString = String.Empty;
+    for(int i = charArr.Length - 1; i > -1; i--)
+        reverseString += charArr[i];
+    return reverseString;
+}
